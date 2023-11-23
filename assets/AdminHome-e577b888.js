c28b61327268f71a7b07e6ac5a71427ca63e112a
QQ9897 <qqqforprogram@gmail.com>
2023-11-23 22:00:56 +0800

    deploy

---
+++ assets/AdminHome-e577b888.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./index-a97ecb40.js";const o={};function r(t,s){return n(),c("h1",null,"這是後台首頁")}const a=e(o,[["render",r]]);export{a as default};
---
+++ assets/AdminOrders-66bf186b.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./index-a97ecb40.js";const n={};function o(t,s){return r(),c("h1",null,"這是後台訂單列頁面")}const a=e(n,[["render",o]]);export{a as default};
